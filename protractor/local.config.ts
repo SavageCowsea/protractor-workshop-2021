eab906a6f57fe10dbf518420835b87db6c5d8544
Eduardo Santos <esantosr@unal.edu.co>
2021-10-26 20:38:04 -0500

    Add reports

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -10,6 +10,10 @@ export const config: Config = {
     browser.ignoreSynchronization  = true;
     reporter();
     browser.manage().timeouts().implicitlyWait(0);
+    const { AwesomeReport } = require('jasmine-awesome-report');
+    const config = {  fullPath: 'reports', fileName: 'awesome', merge: true
+    };
+    jasmine.getEnv().addReporter(AwesomeReport.getReport(config));
 
   }
 };
